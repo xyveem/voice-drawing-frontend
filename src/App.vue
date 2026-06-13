e0a036341b7eecfc952e246e2b1473b6c40fa05f
缪心雨 <1286280961@qq.com>
2026-06-13 21:31:30 +0800

    feat: 新增路由安装配置

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,4 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <router-view />
 </template>
+<style lang="scss"></style>
